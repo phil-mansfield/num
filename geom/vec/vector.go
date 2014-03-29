c730ff9059e7f992c524a0e53495d898fe0695cd
Phil Mansfield <pmansfie@andrew.cmu.edu>
2014-03-29 14:40:17 -0400

    Added documentation to package geom/vec.

--- geom/vec/vector.go
+++ geom/vec/vector.go
@@ -1,8 +1,68 @@
 package vec
 
+/*
+package vec implements basic real-valued vector operations.
+
+All unary operations are implemented as methods while all binary operations are
+implemented as straight functions. Any operation which returns a vector is also
+implemented as an in-place operation whose last arguement is the target and
+whose name now ends with the word "At".
+
+  // returned result
+  result = vec.Cross(v1, v2)
+  // in-place modification of result
+  vec.CrossAt(v1, v2, result)
+
+This latter interface is slightly less conveinent but skips an allocation call,
+allowing for more efficient execution.
+
+All in-place operations are garuanteed to give correct results if the result
+vector is one of the arguments, but may not work correctly if the result
+overlaps with the arguments in other ways.
+
+  base := []float64{ .. }
+  // correct usage
+  vec.AddAt(base[10: 20], base[20: 30], base[10: 20])
+  // incorrect usage
+  vec.AddAt(base[10: 20], base[20: 30], base[15: 25])
+
+Allowing for this behavior would damage performance or require somewhat
+dubious usage of the Unsafe package. (And to be honest I can't imagine very
+many legitimate situations where this would be desirable anyway.)
+
+In the case where a large number of vectors are being stored in an array and
+performance is of prime importance, the user is adviced to make a single
+vector and use SliceIdx to access vectors instead of using an array of Vectors.
+
+  // This example code will fill an array of vectors with copies of v.
+  elemNum := ...
+  v := ... 
+  // This code will consume more memory and result in more cache misses
+  vecsSlow := make([]Vector, elemNum)
+  for i := 0; i < len(vecs); i++ {
+      vecsSlow[i] = vec.Copy(v)
+  }
+  // This code will result in better performance.
+  vecsFast := make([]float64, elemNum * len(v))
+  for i := 0; i < elemNum; i++ {
+      v.CopyAt(vec.SliceIdx(len(v), i))
+  }
+
+Unless otherwise stated, all operations support vectors of all sizes and will
+panic if given vectors of different sizes as arguments.
+*/
+
+import (
+	"math"
+)
+
+// Vector is a type representing an ordered collection of real numbers. It
+// is the type upon which this entire package is built.
+//
+// Vector is implemented as a simple slice to allow for ease of conversion.
 type Vector []float64
 
-
+// Dot computes the dot product of two vectors.
 func Dot(v1, v2 Vector) float64 {
 	if len(v1) != len(v2) {
 		panic("")
@@ -16,13 +76,19 @@ func Dot(v1, v2 Vector) float64 {
 	return sum
 }
 
+// Slice returns a vector corresponding to v[start: end].
 func (v Vector) Slice(start, end int) Vector {
 	return v[start: end]
 }
 
-func (v Vector) IterSlice(width, i int) Vector {
-	start := width * i
-	end := width * (i - 1)
+// IdxSlice returns the vector corresponding to the idxth sub-vector
+// of v which is of length width.
+//
+// The primary use of this method is to emulate accessing an array of
+// vectors while still maintaining favorable cache and memory properties.
+func (v Vector) IdxSlice(width, idx int) Vector {
+	start := width * idx
+	end := width * (idx - 1)
 
 	if start < 0 || end > len(v) {
 		panic("")
@@ -31,6 +97,8 @@ func (v Vector) IterSlice(width, i int) Vector {
 	return v.Slice(start, end)
 }
 
+// Add at computes the sum of two vectors and places the result in a
+// target vector.
 func AddAt(v1, v2, target Vector) {
 	if len(v1) != len(v2) || len(v1) != len(target) {
 		panic("")
@@ -39,6 +107,8 @@ func AddAt(v1, v2, target Vector) {
 	for i := 0; i < len(v1); i++ { target[i] = v1[i] + v2[i] }
 }
 
+// SubAt computes the difference of two vectors and places the result in
+// a target vector.
 func SubAt(v1, v2, target Vector) {
 	if len(v1) != len(v2) || len(v1) != len(target) {
 		panic("")
@@ -47,6 +117,10 @@ func SubAt(v1, v2, target Vector) {
 	for i := 0; i < len(v1); i++ { target[i] = v1[i] - v2[i] }
 }
 
+// CrossAt computes the cross product of two vectors and places the result
+// in a target vector.
+//
+// This function panics if given vectors with lengths other than 3.
 func CrossAt(v1, v2, target Vector) {
 	if len(target) != 3 ||  len(v1) != 3 || len(v2) != 3 {
 		panic("")
@@ -61,32 +135,38 @@ func CrossAt(v1, v2, target Vector) {
 	target[2] = z
 }
 
+// Add returns the sum of two vectors.
 func Add(v1, v2 Vector) Vector {
 	target := make([]float64, len(v1))
 	AddAt(v1, v2, target)
 	return target
 }
 
+// Sub returns the difference of two vectors.
 func Sub(v1, v2 Vector) Vector { 
 	target := make([]float64, len(v1))
 	SubAt(v1, v2, target)
 	return target
 }
 
+// Cross returns the cross product of two vectors.
 func Cross(v1, v2 Vector) Vector { 
 	target := make([]float64, 3)
 	CrossAt(v1, v2, target)
 	return target
 }
 
+// Norm returns the norm of a given vector.
 func (v Vector) Norm() float64 {
 	sum := 0.0
 	for i := 0; i < len(v); i++ {
 		sum += v[i] * v[i]
 	}
-	return sum
+
+	return math.Sqrt(sum)
 }
 
+// CopyAt copies a given vector to a target vector.
 func (v Vector) CopyAt(target Vector) {
 	if len(v) != len(target) {
 		panic("")
@@ -95,6 +175,8 @@ func (v Vector) CopyAt(target Vector) {
 	copy(v, target)
 }
 
+// NormalizeAt computes a norm-1 vector which points in the same direction as
+// a given vector and places the result in a target vector.
 func (v Vector) NormalizeAt(target Vector) {
 	if len(target) != len(v) {
 		panic("")
@@ -107,6 +189,8 @@ func (v Vector) NormalizeAt(target Vector) {
 	}
 }
 
+// Scale at multiplies every element of a given vector by a given scaler
+// and places the result in a target vector.
 func (v Vector) ScaleAt(scaler float64, target Vector) {
 	if len(target) != len(v) {
 		panic("")
@@ -117,18 +201,22 @@ func (v Vector) ScaleAt(scaler float64, target Vector) {
 	}
 }
 
+// Copy returns a copy of the given vector.
 func (v Vector) Copy() Vector {
 	target := make([]float64, len(v))
 	v.CopyAt(target)
 	return target
 }
 
+// Normalize returns a norm-1 vector which points in the same direction as
+// a given vector.
 func (v Vector) Normalize() Vector {
 	target := make([]float64, len(v))
 	v.NormalizeAt(target)
 	return target
 }
 
+// Scale multiplies every element of a given vector by a given scaler.
 func (v Vector) Scale(scaler float64) Vector {
 	target := make([]float64, len(v))
 	v.ScaleAt(scaler, target)
