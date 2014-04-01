74464ce6ca7583ecaeee09a2ce7c38476040dbaa
Phil Mansfield <pmansfie@andrew.cmu.edu>
2014-04-01 00:54:51 -0400

    Finished example muon-cube.go.

--- examples/muon-cube.go
+++ examples/muon-cube.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"math"
 	"os"
 	"strconv"
@@ -11,7 +12,7 @@ import (
 )
 
 const (
-	width = 1.0
+	width = 32.0
 )
 
 var (
@@ -20,9 +21,9 @@ var (
 	bottom = geom.NewPlane(anchorBottom, []float64{0, 0, -1})
 	left = geom.NewPlane(anchorBottom, []float64{0, -1, 0})
 	back = geom.NewPlane(anchorBottom, []float64{-1, 0, 0})
-	top = geom.NewPlane(anchorBottom, []float64{0, 0, 1})
-	right = geom.NewPlane(anchorBottom, []float64{0, 1, 0})
-	front = geom.NewPlane(anchorBottom, []float64{1, 0, 0})
+	top = geom.NewPlane(anchorTop, []float64{0, 0, 1})
+	right = geom.NewPlane(anchorTop, []float64{0, 1, 0})
+	front = geom.NewPlane(anchorTop, []float64{1, 0, 0})
 	sides = []*geom.Plane{top, left, bottom, right, front, back}
 )
 
@@ -32,10 +33,12 @@ func cosSqr(x float64) float64 {
 }
 
 func muonDistance(angles []float64, line *geom.Line, gen *rand.Generator) float64 {
+	line.Anchor[0] =  gen.Uniform(0, width)
+	line.Anchor[1] =  gen.Uniform(0, width)
+	line.Anchor[2] =  gen.Uniform(0, width)
 	angles[0] = gen.MonteCarlo(cosSqr, 0, 2 * math.Pi, 0, 1)
 	angles[1] = gen.Uniform(0, 2 * math.Pi)
 	
-	println(len(angles), len(line.Normal))
 	vec.FromAnglesAt(1, angles, line.Normal)
 	minPos := 2 * width
 	maxNeg := -2 * width
@@ -49,21 +52,72 @@ func muonDistance(angles []float64, line *geom.Line, gen *rand.Generator) float6
 	}
 	return minPos - maxNeg
 }
-	
+
+func cmToMeV(dist float64) float64 {
+	return 110. * math.Pow(dist / 32.0, 2.05)
+}
+
+func MeVToFlux(E float64) float64 {
+	logE := math.Log10(E)
+	var logF float64
+	if logE < 2 {
+		logF = 1e-6 * (logE - 2.0) / 2.0
+	} else if logE > 3 {
+		logF = 1e-6 * (logE - 3.0) * -2.0
+	} else {
+		logF = 1e-6
+	}
+	return math.Pow(10.0, logF)
+}
+
 func main() {
-	if len(os.Args) != 2 {
+	if len(os.Args) != 3 {
 		panic("")
 	}
 
 	trials, err := strconv.Atoi(os.Args[1])
+	binNum, err := strconv.Atoi(os.Args[2])
 	if err != nil { panic(err) }
 	gen := rand.NewTimeSeed(rand.Default)
 
 	angles := make([]float64, 2)
 	line := geom.NewLine([]float64{0.5, 0.5, 0.5}, []float64{0.5, 0.5, 0.5})
 
+	bins := make([]int, binNum)
+	maxDist := math.Sqrt(3) * width
+
 	for i := 0; i < trials; i++ {
 		dist := muonDistance(angles, line, gen)
-		println(dist)
+		binIdx := int(float64(binNum) * dist / maxDist)
+		bins[binIdx]++
 	}
+
+	var percentBins vec.Vector = make([]float64, binNum)
+	for i := 0; i < binNum; i++ {
+		percentBins[i] = float64(bins[i] * binNum) / float64(trials)
+	}
+
+	binWidth := maxDist / float64(binNum)
+	sum := 0.0
+	measuredFluxSum := 0.0
+	lowFluxSum := 0.0
+	fmt.Printf("%15s %15s %15s %15s\n",
+		"# dist [cm]", "E [MeV]", "prob density", "prob integral")
+	for i := binNum - 1; i >= 0; i-- {
+		sum += percentBins[i] / float64(binNum)
+		x := binWidth * (float64(i) + 0.5)
+		E := cmToMeV(x)
+		flux := MeVToFlux(E)
+
+		measuredFluxSum += sum * flux * binWidth
+		if E < 110 {
+			lowFluxSum += flux * binWidth
+		}
+
+		// fmt.Printf("%15.6g %15.6g %15.6g %15.6g\n",
+		// 	x, E, percentBins[i], sum)
+	}
+	println("Full range measured flux:", measuredFluxSum)
+	println("Actual in-range flux:", lowFluxSum)
+	println("Flux ratio:", measuredFluxSum / lowFluxSum)
 }
