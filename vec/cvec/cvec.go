fb753b7d962d268712590f30e6fe36e06f3c3113
Phil Mansfield <pmansfie@andrew.cmu.edu>
2014-05-23 10:14:32 -0400

    vec/ subpackage skeleton.

---
+++ vec/cvec/cvec.go
@@ -0,0 +1,8 @@
+/*
+package cvec implements operation for complex vectors. These operations are
+the same as in num/vec/. 
+
+This is a holdover to make num/mat/ compile.*/
+package cvec
+
+type Vector []complex128
---
+++ vec/vec.go
@@ -0,0 +1,9 @@
+/*
+package vec implements operations for real-valued vectors. These same
+operations are implemented for complex vectors in the subpackage cvec/.
+
+Placeholder package to make sure that the mat/ pacakge 
+*/
+package vec
+
+type Vector []float64
