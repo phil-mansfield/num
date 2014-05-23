ea87c74e5fbad4cb290919b8e573cd9c1b873dbb
Phil Mansfield <pmansfie@andrew.cmu.edu>
2014-05-22 22:20:19 -0400

    Initialization function signatures.

--- mat/mat.go
+++ mat/mat.go
@@ -21,3 +21,41 @@ func (err *MatrixError) Error() string {
 
 // type assertion
 var _ error = &MatrixError{}
+
+// Initialization functions
+
+// New returns a matrix with the given dimensions where all elements are
+// initialized to zero.
+//
+// If height or width is non-positive, an error will be returned.
+func New(width, height int) *Matrix, error {
+	return nil, nil
+}
+
+// Identity returns a square matrix with the given width which contains ones
+// down its diagonal and zeroes everywhere else.
+//
+// If height or width is non-positive, an error will be returned.
+func Identity(width int) *Matrix, error {
+	return nil, nil
+}
+
+// FromArray converts an slice of floats to a matrix with the given dimensions.
+// The element with zero-indexed coordinates (x, y) in the matrix will be the
+// same as that at index data[y * width + x].
+//
+// If width * height != len(data) or if height or width is non-positive, an
+// error will be returned.
+func FromArray(width, height int, data []float64) *Matrix, error {
+	return nil, nil
+}
+
+// FromGrid conversts a 2D slice of floats to a matrix with the same
+// dimensions. The element with zero-indexed coordinates of (x, y) in the
+// matrix will be the same as that at  index data[y][x].
+//
+// If any two rows in data have different lengths, or if len(data) == 0 or
+// len(data[0]) == 0, an error will be returned.
+func FromGrid(data [][]float64) *Matrix, error {
+	return nil, nil
+}
