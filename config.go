4920e6fbeb2b6033bcc8e4878086ba6e11b73b66
Phil Mansfield <pmansfie@andrew.cmu.edu>
2014-05-23 10:11:44 -0400

    Rearranged configuration variables.

---
+++ config.go
@@ -0,0 +1,7 @@
+package num
+
+const (
+	PanicOnError = false
+    ConvergenceIters = 100
+    ConvergenceEpsilon = 3.0e-7
+)
--- converge.go
+++ converge.go
@@ -4,11 +4,6 @@ import (
 	"math"
 )
 
-const (
-	ConvergenceIters = 100
-	ConvergenceEpsilon = 3.0e-7
-)
-
 func CloseEnough(val, diff float64) bool {
 	return math.Abs(diff) <= math.Abs(val * ConvergenceEpsilon)
 }
