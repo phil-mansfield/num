c69b3e71fa3dc567cb86da79268984364a96b1a5
Phil Mansfield <pmansfie@andrew.cmu.edu>
2014-05-23 22:55:52 -0400

    TogglePanic added and slight renaming of standard MatrixError instance names.

--- mat/error.go
+++ mat/error.go
@@ -18,6 +18,21 @@ const (
 	               // aceptable range.
 )
 
+var (
+	willPanic bool = false
+)
+
+// TogglePanic changes the behavior of all functions in package mat when they
+// encounter an error. If they currently return error structs, they will
+// instead panic with the Error string of that struct as a parameter and
+// visa-versa. If the behavior after the funciton has been called is to panic,
+// true is returned, otherwise false is returned.
+//
+// By default operations will not panic.
+func TogglePanic() bool {
+	return false
+}
+
 // Error returns a string representing the first error that occured in the
 // creation of m. If no errors occured or if m is nil, an empty string is
 // returned.
@@ -32,7 +47,7 @@ func (m *Matrix) MatrixError() *MatrixError {
 	return nil
 }
 
-// Error returns a string representation of me. If me is nil, nil is returned.
-func (me *MatrixError) Error() string {
+// Error returns a string representation of err. If me is nil, nil is returned.
+func (err *MatrixError) Error() string {
 	return ""
 }
